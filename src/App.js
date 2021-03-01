cc5b357d0f1b7358ad1c757e7e97b2ce356b91a1
Rostyslav <rostyslavmarchuk440@gmail.com>
2021-03-01 19:20:55 +0200

    Initial database configuration and connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css'
 import { Row, Col, Box } from '@qonsoll/react-design'
+import firestore from './app/constants/firebaseConfig'
+import { useEffect } from 'react'
 
 const App = () => {
   return (
@@ -10,22 +12,5 @@ const App = () => {
     </Box>
   )
 }
-
-/* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
 
 export default App
---
+++ src/app/constants/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_AUTH_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_AUTH_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_AUTH_APP_ID,
+  measurementId: process.env.REACT_APP_AUTH_MEASUREMENT_ID
+}
+
+firebase.initializeApp(FIREBASE_CONFIG)
+
+const firestore = firebase.firestore()
+
+export default firestore
